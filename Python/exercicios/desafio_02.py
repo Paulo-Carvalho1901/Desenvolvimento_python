8a8a115d8390582c6fb93ed0010532e20492fb15
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-01-05 21:05:40 -0300

    feat: desafio 02 concluido

---
+++ Python/exercicios/desafio_02.py
@@ -0,0 +1,9 @@
+# Crie um script Python que leia o
+# dia, o mês e o ano de nascimento de uma
+# pessoa e mostre uma mensagem com a data formatada.
+
+dia = input('Digite o dia: ')
+mes = input('Digite o mês: ')
+ano = input('Digite o ano: ')
+
+print('Você nasceu', dia, 'do mês de', mes, 'do ano de', ano, ',está correto?')
