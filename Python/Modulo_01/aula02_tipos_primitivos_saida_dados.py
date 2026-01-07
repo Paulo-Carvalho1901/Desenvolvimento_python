4b71ec43cec9198971f73c1d9c0c6ba32eb18e3c
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-01-06 23:21:26 -0300

    feat: entendendo o format

--- Python/Modulo_01/aula02_tipos_primitivos_saida_dados.py
+++ Python/Modulo_01/aula02_tipos_primitivos_saida_dados.py
@@ -7,4 +7,4 @@ n2 = int(input('Digite um valor: '))
 s = n1 + n2
 # print('A soma entre', n1, 'e', n2, 'vale', s)
 # função format()
-print('A soma entre {} e {} vale {}'.format(n1,n2,s))
+print('A soma entre {0} e {1} vale {2}'.format(n1, n2, s))
