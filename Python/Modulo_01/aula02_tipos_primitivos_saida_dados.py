aea9f01a292fe8251739cff4fbd286e435fe8cde
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-01-06 23:15:25 -0300

    feat: tipos primitivos int

---
+++ Python/Modulo_01/aula02_tipos_primitivos_saida_dados.py
@@ -0,0 +1,8 @@
+# Tipos primitivos saida de dados
+
+#  Saída de um input sempre será str
+
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite um valor: '))
+s = n1 + n2
+print('A soma vale', s)
