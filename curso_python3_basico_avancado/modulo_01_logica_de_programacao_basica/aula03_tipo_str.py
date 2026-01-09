cb915d12c2353235a89a80fb2f1183f6cbb3b435
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-01-09 15:46:17 -0300

    feat: introdução aos tipos de dados string texto

---
+++ curso_python3_basico_avancado/modulo_01_logica_de_programacao_basica/aula03_tipo_str.py
@@ -0,0 +1,20 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica e forte
+str = string -> texto
+Strings são textos que estão dentro de aspas
+"""
+
+# Aspas simples
+print('Paulo')
+print(1, "Paulo 'Carvalho'")
+
+# Aspas duplas
+print("Paulo")
+print(2, 'Paulo "Carvalho"')
+
+# Escape
+print("Paulo \"Carvalho\"")
+
+# r
+print(r"Paulo \"Carvalho\"")
