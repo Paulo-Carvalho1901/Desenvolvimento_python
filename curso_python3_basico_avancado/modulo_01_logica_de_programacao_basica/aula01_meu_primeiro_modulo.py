49d182da5e1b485a567136c18d5837baf9592189
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-01-09 01:54:14 -0300

    feat: criado docstring comentarios multilinhas

--- curso_python3_basico_avancado/modulo_01_logica_de_programacao_basica/aula01_meu_primeiro_modulo.py
+++ curso_python3_basico_avancado/modulo_01_logica_de_programacao_basica/aula01_meu_primeiro_modulo.py
@@ -1,3 +1,16 @@
 # Comentario de linha 
 print(123) # aqui esta usando uma informação
 # aqui tem outro comentario
+print(123)
+# abaixo
+
+"""
+Docstring
+
+Comentario multilinhas
+"""
+
+'''
+Escrever notas ou explicar o seu código
+
+'''
