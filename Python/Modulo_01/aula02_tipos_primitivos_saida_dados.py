b6b2011a86edf5e654715c039641a413530620a8
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-01-14 02:11:09 -0300

    feat: entendendo tipos primitivos

--- Python/Modulo_01/aula02_tipos_primitivos_saida_dados.py
+++ Python/Modulo_01/aula02_tipos_primitivos_saida_dados.py
@@ -1,10 +1,2 @@
 # Tipos primitivos saida de dados
 
-#  Saída de um input sempre será str
-
-n1 = int(input('Digite um valor: '))
-n2 = int(input('Digite um valor: '))
-s = n1 + n2
-# print('A soma entre', n1, 'e', n2, 'vale', s)
-# função format()
-print('A soma entre {0} e {1} vale {2}'.format(n1, n2, s))
