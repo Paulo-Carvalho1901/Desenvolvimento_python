a593b899eee8b848a9bf6f8d982fccc1ade28f4b
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-01-18 03:29:08 -0300

    feat: (lab) resolvendo exercicios

--- curso_python3_basico_avancado/Labs/lab01_precedencia_opradores/lab01.py
+++ curso_python3_basico_avancado/Labs/lab01_precedencia_opradores/lab01.py
@@ -1,12 +1,20 @@
 # exercicio 01
 
-ex1 = 1 + 2 * 3
-ex2 = (1 + 2) * 3
-ex3 = 2 ** 3 + 1
-ex4 = 2 + 3 ** 2
-ex5 = - 4 / 2
-ex6 = 10 - 4 // 2
+ex1 = 1 + 2 * 3 # 7
+ex2 = (1 + 2) * 3 # 9
+ex3 = 2 ** 3 + 1 # # 9
+ex4 = 2 + 3 ** 2 # 11
+ex5 = - 4 / 2 # - 2
+ex6 = 10 - 4 // 2 # -8
 ex7 = 5 * 2 ** 2
 ex8 = (5 * 2) ** 2
 ex9 = 8 / 2 * 2
 ex10 = 1 + 2 * 3 ** 2
+
+# Resultado de cada operação
+print(ex1)
+print(ex2)
+print(ex3)
+print(ex4)
+print(ex5)
+print(ex6)
