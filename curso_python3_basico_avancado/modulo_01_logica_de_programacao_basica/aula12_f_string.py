40576e96a9fff83242d2fe6de29883df9d3c8b17
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-01-24 03:37:26 -0300

    feat: format

---
+++ curso_python3_basico_avancado/modulo_01_logica_de_programacao_basica/aula12_f_string.py
@@ -0,0 +1,10 @@
+# Tudo em Python é um objeto
+# Método é tudo aquilo que executa uma determinada ação
+
+a = 'A'
+b = 'B'
+c = 1.1
+
+formato = 'a={} b={} c={:.2f}'.format(a, b, c)
+
+print(formato)
