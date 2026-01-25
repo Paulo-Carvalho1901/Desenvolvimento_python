81e2a45e94324b63b5986b4ad86a734de818ad24
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-01-25 03:16:12 -0300

    feat: entendendo fluxo do if else

---
+++ curso_python3_basico_avancado/modulo_01_logica_de_programacao_basica/aula15_if_elif_else_fluxo.py
@@ -0,0 +1,12 @@
+# if / elif      / else
+# se / se não se / se não
+
+condicao = False
+
+if condicao:
+    print('Este é o código no if.')
+else:
+    print('Esse é else do primeiro if')
+
+if 10 == 10:
+    print('Outro if')
