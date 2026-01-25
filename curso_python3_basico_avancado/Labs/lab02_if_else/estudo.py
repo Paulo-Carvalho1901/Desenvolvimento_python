2fafeab8e274c5bedc1c8a18b0e78df36eb1e7b3
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-01-25 04:45:08 -0300

    feat: o que é uma condição

---
+++ curso_python3_basico_avancado/Labs/lab02_if_else/estudo.py
@@ -0,0 +1,12 @@
+# Estruturas Condicionais em Python (Guia Completo)
+
+# As estruturas condicionais são fundamentais em qualquer linguagem de programação, pois permitem tomar
+# decisões com base em condições lógicas. Em Python, trabalhamos principalmente com:
+
+# if
+# elif
+# else
+
+# Este guia traz explicações claras, exemplos, boas práticas e pegadinhas comuns.
+
+# 1. O que é uma condição?
