c86e1e599743607aaf9e6d75bc271116327a8f33
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-02-22 15:54:20 -0300

    (ex03) adicionando um condição igual

--- curso_python3_basico_avancado/exercicios/ex03.py
+++ curso_python3_basico_avancado/exercicios/ex03.py
@@ -1,7 +1,7 @@
 primeiro_valor = input('Digite um valor: ')
 segundo_valor = input('Digite outro valor: ')
 
-if primeiro_valor > segundo_valor:
-    print(f'O {primeiro_valor=} é maior que o {segundo_valor=}')
+if primeiro_valor >= segundo_valor:
+    print(f'O {primeiro_valor=} é maior ou igual {segundo_valor=}')
 else:
-    print(f'O {segundo_valor=} é maior que o {primeiro_valor=}')
+    print(f'O {segundo_valor=} é maior ou igual {primeiro_valor=}')
