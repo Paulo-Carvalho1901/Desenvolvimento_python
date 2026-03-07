918e075ee98bcbed360b8cb1a96c617aa853aa51
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-03-07 12:17:04 -0300

    (Iniciante) fatiamento de strings

---
+++ curso_python3_basico_avancado/modulo_01_logica_de_programacao_basica/aula23_fatiamento_strings.py
@@ -0,0 +1,18 @@
+"""
+Fatiamento de strings
+ 012345678
+ Olá mundo
+-987654321
+Fatiamento [i:f:p] [::]
+Obs.: a função len retorna a qtd 
+de caracteres da str
+"""
+variavel = 'Olá mundo'
+print(variavel[5])
+print(variavel[-4])
+print(variavel[4:])
+print(variavel[:5])
+print(variavel[-8:-2])
+print(variavel[0:9:1])
+print(variavel[::-1]) # invertendo string
+print(len(variavel)) # qtd de caractere
