a81b3bbddd6181a59e2af6804ebf6418a74d70b4
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-03-07 20:12:52 -0300

    concluido exercicio 3

--- Python/exercicios/desafio_03.py
+++ Python/exercicios/desafio_03.py
@@ -1,3 +1,9 @@
 # Crie um programa que leia dois
 # números e mostre a soma entre eles
 
+numero_1 = int(input('Digite um número: '))
+numero_2 = int(input('Digite outro número: '))
+
+soma = numero_1 + numero_2
+
+print(f'A soma de {numero_1} + {numero_2} = {soma}')
