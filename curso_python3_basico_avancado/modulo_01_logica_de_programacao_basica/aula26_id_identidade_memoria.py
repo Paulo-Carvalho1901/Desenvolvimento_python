679da6aa3bb1d206c8ae8917cc4d4b475a8b1b2e
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-03-07 22:52:25 -0300

    (inicianrte) id identificador na memória

---
+++ curso_python3_basico_avancado/modulo_01_logica_de_programacao_basica/aula26_id_identidade_memoria.py
@@ -0,0 +1,11 @@
+"""
+Flag (Bandeira) - Marca um local
+None = Não Valor
+is e is not = é ou não é (tipo, valor, identidade)
+id = Identidade
+"""
+
+v1 = 'a'
+v2 = 'a'
+print(id(v1))
+print(id(v2))
