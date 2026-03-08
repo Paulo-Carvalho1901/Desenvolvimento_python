1e567552b0744ec8740b743ac7f909bbfef9cc98
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-03-08 17:24:18 -0300

    (refector) solução colocado a variavel que converte a para inteiro dentro do bloco if

--- curso_python3_basico_avancado/exercicios/ex05.py
+++ curso_python3_basico_avancado/exercicios/ex05.py
@@ -4,11 +4,25 @@ informe se este número é par ou impar. Caso o úsuario não digite um número
 inteiro, informe que não é um número inteiro.
 """
 
+# numero = input('Digite um número: ')
+
+# numero_int = int(numero)
+
+# if numero.isdigit():
+#     if numero_int % 2 == 0:
+#         print(f'Seu número digitado "{numero_int}" é PAR...')
+#     else:
+#         print(f'Seu número digitado "{numero_int}" IMPAR...')
+# else:
+#     print('Favor digite apenas números inteiros.')
+
+# Outra solução
+
+
 numero = input('Digite um número: ')
 
-numero_int = int(numero)
-
 if numero.isdigit():
+    numero_int = int(numero)
     if numero_int % 2 == 0:
         print(f'Seu número digitado "{numero_int}" é PAR...')
     else:
