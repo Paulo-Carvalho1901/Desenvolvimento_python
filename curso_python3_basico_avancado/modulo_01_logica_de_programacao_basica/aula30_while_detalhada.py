a5d2186767aaa9e9bc57db47145f909cb7774d41
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-03-16 01:52:07 -0300

    (while) detalhando o while

---
+++ curso_python3_basico_avancado/modulo_01_logica_de_programacao_basica/aula30_while_detalhada.py
@@ -0,0 +1,14 @@
+"""
+Repetições
+while (enquanto)
+Executa uma ação enquanto a condiçao for verdadeira
+Loope infinito -> Quando um código não tem fim
+"""
+
+contador = 0
+
+while contador <= 10: # condição
+    contador = contador + 1 # incrementando o contador
+    print(contador) # mostrando o contador
+
+print('ACABOU!') # FORA DO WHILE
