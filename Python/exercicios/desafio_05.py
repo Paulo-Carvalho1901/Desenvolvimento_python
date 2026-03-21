bda328e2ca4bb56bca3b4d4ede94c255ed9dc797
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-03-21 17:40:33 -0300

    resolvido: exercicio 5

---
+++ Python/exercicios/desafio_05.py
@@ -0,0 +1,9 @@
+"""
+Faça um programa que leia um número
+inteiro e mostre na tela o seu sucessor e seu
+antecessor.
+"""
+
+numero = int(input('Digite um número: '))
+
+print(f'O seu número é {numero} e seu antecessor é {numero - 1} e seu sucessor é {numero + 1}')
