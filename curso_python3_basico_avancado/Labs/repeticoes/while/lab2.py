6bee72cb8048cae5508797eded4ec00ce0ab661c
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-03-24 15:22:09 -0300

    segunda solução do problema com if

--- curso_python3_basico_avancado/Labs/repeticoes/while/lab2.py
+++ curso_python3_basico_avancado/Labs/repeticoes/while/lab2.py
@@ -15,12 +15,26 @@
 # Exercício 1
 # Peça para o usuário digitar números até que ele digite **0**. Ao final, exiba a soma de todos os números digitados.
 
+# soma = 0
+# numero = int(input('Digite um número digite "0" para sair: '))
+
+# while numero != 0:
+#     soma += numero
+#     numero = int(input('Digite um número digite "0" para sair: '))
+
+# print(f'A soma dos números diggitados é: {numero}')
+
+# Segunda solução com if
+
 soma = 0
 numero = int(input('Digite um número digite "0" para sair: '))
 
-while numero != 0:
-    soma += numero
+while True:
     numero = int(input('Digite um número digite "0" para sair: '))
 
-print(f'A soma dos números diggitados é: {numero}')
+    if numero == 0:
+        break
 
+    soma += numero
+
+print(f'A soma dos números é {soma}')
