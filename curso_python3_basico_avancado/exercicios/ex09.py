2faeb1659f56acd23af40d5a41914d594b3e7a27
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-03-27 00:31:49 -0300

    add varios comentarios para estudo

--- curso_python3_basico_avancado/exercicios/ex09.py
+++ curso_python3_basico_avancado/exercicios/ex09.py
@@ -10,7 +10,6 @@ logo abaixo uma condição de parada break
 
 
 while True:
-
     # solicitando os dados para úsuario
     numero_1 = input('Digite um número: ')
     numero_2 = input('Digite outro número: ')
@@ -18,31 +17,39 @@ while True:
 
     numeros_validos = None # Bandeira (flag)
 
+    # Aqui um ponto importante, poderia no lugar de try, except 
+    # utilizar isdigit() para validar se que o úsuario enviou é realmente um número
+
     # tratando erros
     try:
         numero_1_flot = float(numero_1)
         numero_2_flot = float(numero_2)
-        numeros_validos = True
+        numeros_validos = True # se chegar aqui quer dizer que os números são validos
     except:
         numeros_validos = None
 
-    # condição
+    # condição nessa condição checa, se os números são validos 
+    # se os números forem iguais ao None é números são invalidos
     if numeros_validos is None:
         print('Um ou ambos os números digitados não são validos.')
-        continue
+        continue # volta no inicio pedindo para solicitar os números novamente
 
     operadores_permitidos = '+-/*'
 
+    # Aqui testa a condição se o operador operador nao esta em operador_permitidos
+    # se o valor não estiver "operadores permitidos"
     if operador not in operadores_permitidos:
         print('Operador invalido.')
-        continue
+        continue # volta no inicio pedindo para solicitar os números novamente
 
+    # Aqui testa a lógica se tamanho do operador > 1
+    # digite apenas 1 operador garantindo essa condição
     if len(operador) > 1:
         print('Digite apenas 1 operador')
-        continue
+        continue # volta no inicio pedindo para solicitar os números novamente
 
     sair = input('Quer sair? [s]im: ').lower().startswith('s')
     
     if sair is True:
-        break
+        break # para o laço while quando "sair" for True
 
