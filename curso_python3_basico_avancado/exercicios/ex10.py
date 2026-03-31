b9322c8cbd0ce13a8ab90c45a64dc225dd75e5a3
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-03-31 00:41:50 -0300

    exercicio 10 contando quantas vezes letra apareceu mais vezes

---
+++ curso_python3_basico_avancado/exercicios/ex10.py
@@ -0,0 +1,27 @@
+frase = 'O Python é uma linguagem de programação '\
+    'multiparadigma. ' \
+    'Python foi criado por Guido van Rossum'
+
+i = 0
+quantidade_apareceu_mais_vezes = 0
+letra_apareceu_mais_vezes = ''
+
+while i < len(frase):
+    letra_atual = frase[i]
+    quantidade_apareceu_mais_vezes_atual = frase.count(letra_atual)
+    
+    if letra_atual == ' ':
+        i += 1
+        continue
+
+    if quantidade_apareceu_mais_vezes < quantidade_apareceu_mais_vezes_atual:
+        quantidade_apareceu_mais_vezes = quantidade_apareceu_mais_vezes_atual
+        letra_apareceu_mais_vezes = letra_atual
+
+    i += 1
+
+print(
+    'A letra que apareceu mais vezes foi '
+    f'"{letra_apareceu_mais_vezes}" que apareceu '
+    f'{quantidade_apareceu_mais_vezes}x'
+)
