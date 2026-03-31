8f4c8dcaca66be16c379e705175397035ab8ee88
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-03-31 00:18:16 -0300

    Aula34 while com else

---
+++ curso_python3_basico_avancado/modulo_01_logica_de_programacao_basica/aula34_while_else.py
@@ -0,0 +1,16 @@
+# while/else
+
+string = 'valor qualquer'
+
+i = 0
+while i < len(string):
+    letra = string[i]
+
+    if letra == ' ':
+        break
+
+    print(letra)
+    i += 1
+else:
+    print('Não entrei um espaço na string')
+print('Fora do while')
